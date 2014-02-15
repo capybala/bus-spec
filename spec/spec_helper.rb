b282fabeb750e403bed400cd294997e485dafaad
orangain <orangain@gmail.com>
2014-02-15 15:42:03 +0900

    Extend feature

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,14 @@ Dir.glob('spec/steps/*steps.rb') { |f| load f, true }
 require 'capybara/dsl'
 require 'capybara/rspec'
 require 'capybara/webkit'
+
+def simulate_location(lat, lng, accuracy=0)
+  page.execute_script <<-EOS
+    window.navigator.geolocation = {
+      getCurrentPosition: function(success){
+        var position = {coords: {latitude: #{lat}, longitude: #{lng}, accuracy: #{accuracy}}};
+        success(position);
+      }
+    };
+  EOS
+end
--- spec/steps/mobile_steps.rb
+++ spec/steps/mobile_steps.rb
@@ -6,10 +6,28 @@ step 'サイトにアクセスする' do
   Capybara.run_server = false
 end
 
+step ':placeに居る' do |place|
+  case place
+  when '京都市役所'
+    @place = {lat: 35.01129870069897, lng: 135.76807713296512}
+  else
+    raise "Unknown place"
+  end
+end
+
 step 'トップページを表示する' do
   visit '/'
+  simulate_location(@place[:lat], @place[:lng]) if @place
 end
 
 step '画面に近くのバス停を探すボタンが表示されていること' do
-  expect(page).to have_xpath('id("btn")')
+  expect(page).to have_link('近くのバス停を探す')
+end
+
+step 'バス停を探すボタンをクリックする' do
+  click_link '近くのバス停を探す'
+end
+
+step '画面に:textと表示されていること' do |text|
+  expect(page).to have_content(text)
 end
